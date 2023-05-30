cc1cabe4dc5879e82f1175c39da041e33f106f55
Mari <mari.philon@gmail.com>
2023-05-30 08:43:03 +0300

    init

---
+++ fooditem.js
@@ -0,0 +1,178 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Sashimi',
+    category : 'senator',
+    rating : 4.3,
+    price: 13,
+    img: 'images/1.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Nigirizushi',
+    category : 'senator',
+    rating : 4.3,
+    price: 15,
+    img: 'images/2.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Ebi Nigiri',
+    category : 'senator',
+    rating : 4.3,
+    price: 18,
+    img: 'images/3.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Tamago Nigiri',
+    category : 'senator',
+    rating : 4.3,
+    price: 15,
+    img: 'images/4.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Unagi Nigiri',
+    category : 'senator',
+    rating : 4.3,
+    price: 10,
+    img: 'images/5.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Sake Nigiri',
+    category : 'senator',
+    rating : 4.3,
+    price: 12,
+    img: 'images/6.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Chicken pot pie',
+    category : 'nippet',
+    rating : 4.3,
+    price: 13,
+    img: 'images/7.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Mashed potatoes',
+    category : 'nippet',
+    rating : 4.3,
+    price: 20,
+    img: 'images/8.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Fried chicken',
+    category : 'nippet',
+    rating : 4.3,
+    price: 15,
+    img: 'images/9.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken soup',
+    category : 'nippet',
+    rating : 4.3,
+    price: 11,
+    img: 'images/6.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Meatloaf',
+    category : 'nippet',
+    rating : 4.3,
+    price: 10,
+    img: 'images/7.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Lasagna',
+    category : 'meer',
+    rating : 4.3,
+    price: 14,
+    img: 'images/8.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Spaghetti',
+    category : 'meer',
+    rating : 4.3,
+    price: 12,
+    img: 'images/9.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Onion rings',
+    category : 'meer',
+    rating : 4.3,
+    price: 17,
+    img: 'images/10.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Breadsticks',
+    category : 'veggi',
+    rating : 4.3,
+    price: 20,
+    img: 'images/11.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Pigs in a blanket',
+    category : 'veggi',
+    rating : 4.3,
+    price: 15,
+    img: 'images/12.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'French fries',
+    category : 'veggi',
+    rating : 4.3,
+    price: 10,
+    img: 'images/13.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Mac & cheese',
+    category : 'veggi',
+    rating : 4.3,
+    price: 15,
+    img: 'images/11.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Cheddar Biscuits',
+    category : 'veggi',
+    rating : 4.3,
+    price: 12,
+    img: 'images/15.jpg',
+    quantity: 1
+
+},
+]
+
+export {foodItem};
